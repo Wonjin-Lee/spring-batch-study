ee12055a779fcac6a118a5113854546e7f941220
Wonjin-Lee <claude17@naver.com>
2022-11-15 01:12:12 +0900

    거래명세서 출력을 위한 배치 프로젝트 생성

---
+++ statement/src/test/java/io/ader/batch/statement/StatementApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ader.batch.statement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
